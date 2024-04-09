591b15bb91b9375f1af7502c073dcb7ea276d3e6
rmsh5413 <rmshthapa987@gmail.com>
2024-04-09 15:08:39 +0545

    updae

--- portfolio/app/build_files.sh
+++ portfolio/app/build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python5.0.3 -m pip install -r requirements.txt
-python5.0.3 manage.py collectstatic --noinput --clear
+python3.12.2 -m pip install -r requirements.txt
+python3.12.2 manage.py collectstatic --noinput --clear
 echo "BUILD END"
